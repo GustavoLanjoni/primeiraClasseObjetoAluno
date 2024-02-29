b1297216094f8f6536325b28d58baa18a1c3f547
Gustavo <gustavosilva94514@gmail.com>
2024-02-29 20:11:27 -0300

    first commit

---
+++ src/primeiraClasseObjetoAluno.java
@@ -0,0 +1,19 @@
+public class primeiraClasseObjetoAluno {
+    public static void main(String[] args) throws Exception {
+        
+            String nome;
+            int idade;
+            String dataNascimento;
+            String registroGeral;
+            String numeroCpf;
+            String nomeMae;
+            String dataMatricula;
+                
+                
+
+
+
+
+
+    }
+}
