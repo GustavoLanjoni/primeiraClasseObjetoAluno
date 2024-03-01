23c1f52941b1c682ed1708aab86cc209711ac12b
Gustavo <gustavosilva94514@gmail.com>
2024-03-01 01:15:23 -0300

    Projeto de Calculo do aluno

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,4 +1,4 @@
-public class teste {
+public class Aluno {
     private String nome;
     private int idade;
     private String dataNascimento;
@@ -6,9 +6,47 @@ public class teste {
     private String numeroCpf;
     private String nomeMae;
     private String dataMatricula;
-    
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double nota4;
+
+
     // Construtor padrão
-    public teste() {
+    public double getNota1(){
+        return nota1;
+    }
+
+    public void setNota1(double nota1){
+        this.nota1 = nota1;
+    }
+
+    public double getNota2 (){
+        return nota2;
+    }
+
+    public void setNota2(double nota2){
+        this.nota2 = nota2;
+    }
+
+    public double getNota3(){
+        return nota3;
+    }
+
+    public void setNota3(double nota3){
+        this.nota3 = nota3;
+    }
+
+    public double getNota4(){
+        return nota4;
+    }
+
+    public void setNota4(double nota4){
+        this.nota4 = nota4;
+    }
+
+
+    public Aluno() {
         this.nome = "";
         this.idade = 0;
         this.dataNascimento = "";
@@ -19,12 +57,12 @@ public class teste {
     }
 
     // Construtor com um parâmetro para nome
-    public teste(String nome) {
+    public Aluno(String nome) {
         this.nome = nome;
     }
 
     // Construtor com dois parâmetros para nome e idade
-    public teste(String nome, int idade) {
+    public Aluno(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
@@ -86,16 +124,32 @@ public class teste {
         this.dataMatricula = dataMatricula;
     }
 
+    /*Calculo da media */
+
+    public double getNota(){
+        return (nota1 + nota2 + nota3 + nota4 ) / 4;
+    }
+
+  
+
+
+
+
     // Método main para exemplo de uso
     public static void main(String[] args) {
         // Exemplo de uso dos construtores
-        teste teste1 = new teste(); // Chama o construtor padrão
-        teste teste2 = new teste("João"); // Chama o construtor com um parâmetro para nome
-        teste teste3 = new teste("Maria", 25); // Chama o construtor com dois parâmetros para nome e idade
+        Aluno aluno1 = new Aluno(); // Chama o construtor padrão
+        Aluno aluno2 = new Aluno("João"); // Chama o construtor com um parâmetro para nome
+        Aluno aluno3 = new Aluno("Maria", 25); // Chama o construtor com dois parâmetros para nome e idade
 
         // Exemplo de uso dos métodos getters e setters
-        teste1.setNome("Fernando");
-        teste1.setIdade(30);
-        System.out.println("Nome: " + teste1.getNome() + ", Idade: " + teste1.getIdade());
+        aluno1.setNome("Fernando");
+        aluno1.setIdade(30);
+        aluno1.setNota1(90);
+        aluno1.setNota2(70);
+        aluno1.setNota3(90);
+        aluno1.setNota4(100);
+        System.out.println("Nome: " + aluno1.getNome() + ", Idade: " + aluno1.getIdade());
+        System.out.println("Media da nota e = " + aluno1.getNota());
     }
 }
--- src/primeiraClasseObjetoAluno.java
+++ src/primeiraClasseObjetoAluno.java
@@ -1,20 +1,20 @@
 public class primeiraClasseObjetoAluno {
     public static void main(String[] args) throws Exception {
         
-        teste teste1; // Declaração do objeto, mas não inicializado
+        Aluno aluno1; // Declaração do objeto, mas não inicializado
 
-        teste teste2 = new teste(); // Inicialização de um objeto utilizando o construtor padrão
+        Aluno aluno2 = new Aluno(); // Inicialização de um objeto utilizando o construtor padrão
 
-        teste teste3 = new teste(); // Inicialização de outro objeto utilizando o construtor padrão
+        Aluno aluno3 = new Aluno(); // Inicialização de outro objeto utilizando o construtor padrão
 
-        teste teste4 = new teste("Gustavo"); // Inicialização de um objeto utilizando um construtor com um parâmetro para nome
+        Aluno aluno4 = new Aluno("Gustavo"); // Inicialização de um objeto utilizando um construtor com um parâmetro para nome
       
-        teste teste5 = new teste("Jose", 50); // Inicialização de um objeto utilizando um construtor com dois parâmetros para nome e idade
+        Aluno aluno5 = new Aluno("Jose", 50); // Inicialização de um objeto utilizando um construtor com dois parâmetros para nome e idade
 
+                
 
         // Exemplo de uso dos objetos
-        System.out.println("Nome de teste4: " + teste4.getNome());
-        System.out.println("Nome de teste5: " + teste5.getNome() + ", Idade: " + teste5.getIdade());
+        System.out.println("Nome de aluno4: " + aluno4.getNome());
+        System.out.println("Nome de aluno5: " + aluno5.getNome() + ", Idade: " + aluno5.getIdade());
     }
 }
-
